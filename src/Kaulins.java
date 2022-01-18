2679e228d29be49b97ac77d6b7caa6ec2f0b52ee
Porins <porinslauris@gmail.com>
2022-01-18 10:37:25 +0200

    Ieviesta metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,11 +1,13 @@
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
+	static void mestKaulinu(){
 		Random rand = new Random();
 		int skaitlis = rand.nextInt(7)+1;
 		System.out.println("Uzskrita "+skaitlis);
+	}
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
