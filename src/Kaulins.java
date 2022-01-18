850b2e825f3d89bec59eaae1581ff07a0344b883
Porins <porinslauris@gmail.com>
2022-01-18 11:08:34 +0200

    Iespējams ievadīt, cik reizes mest kauliņu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,11 +1,17 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
-
 	public static void main(String[] args) {
 		Random rand = new Random();
-		int skaitlis = rand.nextInt(7)+1;
-		System.out.println("Uzskrita "+skaitlis);
+		Scanner dati = new Scanner(System.in);
+		
+			System.out.println("Cik reizes mest kauliņu");
+			int reizes = dati.nextInt();
+		for(int i=1; i<=reizes; i++){
+			int skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzskrita "+skaitlis);
+		}
+		dati.close();
 	}
-
 }
